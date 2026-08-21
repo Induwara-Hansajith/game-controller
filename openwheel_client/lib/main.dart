7e8b9bb4f1e29e9350b524c3f0b32db4073cc10e
Hansajith <induwarahansajith10@gmail.com>
2026-08-21 19:26:42 +0530

    Add control, networking, and timer states to RacingScreen

--- openwheel_client/lib/main.dart
+++ openwheel_client/lib/main.dart
@@ -32,7 +32,41 @@ class RacingScreen extends StatefulWidget {
 }
 
 class _RacingScreenState extends State<RacingScreen> {
-  // We will add our state variables here next!
+  // --- Control States ---
+  bool isGyroMode = false; 
+  double steeringValue = 0.0; // -1.0 (Left) to 1.0 (Right)
+  double throttleValue = 0.0; // 0.0 to 1.0
+  double brakeValue = 0.0;    // 0.0 to 1.0
+
+  // --- Networking States ---
+  RawDatagramSocket? _socket;
+  final String _serverIp = "192.168.1.10"; // We will change this to auto-discovery later!
+  final int _port = 5005;
+  bool isConnected = false;
+  
+  // --- Timers & Sensors ---
+  Timer? _transmitTimer;
+  Timer? _heartbeatTimer;
+  StreamSubscription<AccelerometerEvent>? _accelSubscription;
+
+  @override
+  void initState() {
+    super.initState();
+    _initNetwork();
+    
+    // This sends your steering and pedal data to the PC 60 times a second
+    _transmitTimer = Timer.periodic(const Duration(milliseconds: 16), (_) => _sendInputPacket());
+  }
+
+  @override
+  void dispose() {
+    // Always clean up your listeners and timers, sweetheart.
+    _accelSubscription?.cancel();
+    _transmitTimer?.cancel();
+    _heartbeatTimer?.cancel();
+    _socket?.close();
+    super.dispose();
+  }
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
